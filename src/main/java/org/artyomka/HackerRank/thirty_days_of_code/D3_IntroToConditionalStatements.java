33125f7df0ace868d46bf24fa56ad18f8c803cf2
Artyom Pionov <pionovartem11@gmail.com>
2023-09-30 21:20:29 +0100

    New Commit: 3rd Day of 30 days done + Completed the last challenges in Introduction.

--- src/main/java/org/artyomka/HackerRank/thirty_days_of_code/D3_IntroToConditionalStatements.java
+++ src/main/java/org/artyomka/HackerRank/thirty_days_of_code/D3_IntroToConditionalStatements.java
@@ -1,22 +1,19 @@
-package org.artyomka.HackerRank.Introduction;
-
-import org.artyomka.Annotations.Author;
+package org.artyomka.HackerRank.thirty_days_of_code;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-@SuppressWarnings("DuplicatedCode")
-@Author("Artyomka")
-public class JavaIfElse {
-    @SuppressWarnings("ConstantValue")
+@SuppressWarnings({"DuplicatedCode", "ConstantValue"})
+public class D3_IntroToConditionalStatements {
     public static void main(String[] args) throws IOException {
         // Input With Try-with-resources
-        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))){
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+            // Input
             int N = Integer.parseInt(bufferedReader.readLine().trim());
 
             // Logic
-            if(N % 2 != 0){
+            if (N % 2 != 0) {
                 System.out.println("Weird");
             } else if (N % 2 == 0 && (N > 2 && N < 6)) {
                 System.out.println("Not Weird");
